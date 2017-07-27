d9c8b45ea9b20b4aedb750d6d31e3b4d7ad0ec52
James Theo <jamestheo@Jamess-MacBook-Pro.local>
2017-07-28 00:58:34 +1000

    fixed problem with grip strength and grab strength data not showing. need to fix joint position data

--- js/main.js
+++ js/main.js
@@ -2,30 +2,76 @@
 
 $( document ).ready(function() {
 
-  function concatData(id, data) {
+  var concatData = function(id, data) {
     return id + ": " + data + "<br>"
   }
 
+  function getFingerName(fingerType){
+    switch (fingerType) {
+      case 0:
+        return 'Thumb';
+      break;
+
+      case 1:
+        return 'Index';
+      break;
+
+      case 2:
+        return 'Middle';
+      break;
+
+      case 3:
+        return 'Ring';
+      break;
+
+      case 4:
+        return 'Pinky';
+      break;
+    }
+  }
+
+  function concatJointPosition(id, position){
+    return id + ": " + position[0] + ", " + position[1] + ", " + position[2] + "<br>"
+  }
+
   var output = document.getElementById('output');
-  var framestring = "", handstring = "", fingerstring = "";
+  var frameString = "", handString = "", fingerString = "";
   var hand, finger;
 
   var options = { enableGestures: true};
 
   Leap.loop(options, function(frame){
-    framestring = concatData("frame_id", frame.id);
-    framestring += concatData("num_hands", frame.hands.length);
-    framestring += concatData("num_fingers", frame.fingers.length);
-    framestring += "<br>";
+    frameString = concatData("frame_id", frame.id);
+    frameString += concatData("num_hands", frame.hands.length);
+    frameString += concatData("num_fingers", frame.fingers.length);
+    frameString += "<br>";
 
     // having a fit over the extra argument in for loop. WTF does it even mean?
-    for (var i = 0; len = frame.hands.length; i < len; i++) {
+    for (var i = 0; i < frame.hands.length; i++) {
       hand = frame.hands[i];
-      handstring = concatData("hand_type", hand.type);
-      framestring += handstring;
+      handString = concatData("hand_type", hand.type);
+      handString += concatData("pinch_strength", hand.pinchStrength);
+      handString += concatData("grab_strength", hand.grabStrength);
+      handString += concatData("confidence", hand.confidence);
+
+      handString += '<br>'
+
+      for (var j = 0; j < hand.fingers.length; j++) {
+        finger = hand.fingers[j];
+        fingerString += concatData("finger_type", finger.type) + " (" + getFingerName(finger.type) + ")"
+        fingerString += concatData("finger_dip", finger.dipPosition);
+        fingerString += concatData("finger_pip", finger.pipPosition);
+        fingerString += concatData("finger_mcp", finger.mcpPosition);
+
+        fingerString += '<br>'
+
+      }
+
+      frameString += handString;
+      frameString += fingerString;
     }
 
-    output.innerHTML = framestring;
+    output.innerHTML = frameString;
 
   })
 
