2fd4d74422e17b7830f74a5b5ac0c8a3109d8023
James Theo <jamestheo@Jamess-MacBook-Pro.local>
2017-08-02 14:59:09 +1000

    hands now render to browser. Gestures for "I" "O" "U" still not being recognised properly. YOLO

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 
 $( document ).ready(function() {
 
+
   function voicePlay(text){
     if(responsiveVoice.voiceSupport()) {
     responsiveVoice.speak(text);
     }  //checks to see if browser supports text to speech
   }  //close voicePlay function - converts string to audible speech
-
-
 
   var output = document.getElementById('output');
   var frameString = "", handString = "", fingerString = "";
@@ -18,7 +17,8 @@ $( document ).ready(function() {
 
 
 
-  Leap.loop(function(frame){
+  // Leap.loop(function(frame){
+  var processFrame = function(frame){
 
     // var crossProduct = Leap.vec3.create();
     // var direction = hand.direction;
@@ -26,12 +26,7 @@ $( document ).ready(function() {
     //
     // Leap.vec3.cross(crossProduct, direction, normal);
 
-    frameString = concatData("frame_id", frame.id);
-    frameString += concatData("num_hands", frame.hands.length);
-
     if( frame.hands.length > 1 ){
-      frameString += concatData("hands[0].type", frame.hands[0].type);
-      frameString += concatData("hands[1].type", frame.hands[1].type);
 
       if( frame.hands[0].type === "left") {
         l = frame.hands[0];
@@ -42,7 +37,6 @@ $( document ).ready(function() {
       }
 
     } else if( frame.hands.length > 0 ){
-      frameString += concatData("hands[0].type", frame.hands[0].type);
 
       if( frame.hands[0].type === "left") {
         l = frame.hands[0];
@@ -57,18 +51,17 @@ $( document ).ready(function() {
       r = null;
     }
 
-    frameString += concatData("num_fingers", frame.fingers.length);
-    frameString += "<br>";
-
-
-
-
     if(l && r){ // both l & r visible
-
       var lf = l.fingers;
       var rf = r.fingers;
       var lExtended = lf[0].extended && lf[1].extended && lf[2].extended && lf[3].extended && lf[4].extended
       var rExtended = rf[0].extended && rf[1].extended && rf[2].extended && rf[3].extended && rf[4].extended
+
+  //       var xdistT = lf[1].mcpPosition[0] - rf[0].tipPosition[0];
+  //       var ydistT = lf[1].mcpPosition[1] - rf[0].tipPosition[1];
+  //       var zdistT = lf[1].mcpPosition[2] - rf[0].tipPosition[2];
+  //       var tDist = Math.sqrt( xdistT*xdistT + ydistT*ydistT + zdistT*zdistT );
+  // console.log(tDist);
 
       if (lExtended) {
         if (rf[1].extended && !rf[0].extended && !rf[2].extended && !rf[3].extended && !rf[4].extended) {
@@ -78,99 +71,122 @@ $( document ).ready(function() {
           var zdist = lf[0].tipPosition[2] - rf[1].tipPosition[2];
           var dist = Math.sqrt( xdist*xdist + ydist*ydist + zdist*zdist );
 
-          console.log(dist);
           if ( dist < 20 ){
             frameString += 'YOU MADE THE LETTER "A" MOUTHFUCKER' + dist + '<br>';
             text = 'A'
             voicePlay(text)
           }
         } //LETTER A
-      }
 
-      // if(//LETTER A
-      //   // all finger on L hand extended and index on R hand extended.
-      //   (lf[0].extended && lf[1].extended && lf[2].extended && lf[3].extended && lf[4].extended && rf[1].extended)
-      //   // no other fingers on R extended
-      //   && (!rf[0].extended && !rf[2].extended && !rf[3].extended && !rf[4].extended)
-      //   ){
-      //   var xdist = lf[0].tipPosition[0] - rf[1].tipPosition[0];
-      //   var ydist = lf[0].tipPosition[1] - rf[1].tipPosition[1];
-      //   var zdist = lf[0].tipPosition[2] - rf[1].tipPosition[2];
-      //   var dist = Math.sqrt( xdist*xdist + ydist*ydist + zdist*zdist );
-      //   console.log(dist);
-      //   if ( dist < 20 ){
-      //     frameString += 'YOU MADE THE LETTER "A" MOUTHFUCKER' + dist + '<br>';
-      //     text = 'A'
-      //     voicePlay(text)
-      //   }
-      //
-      //   }
-        if ( lf[2].extended && lf[3].extended && lf[4].extended && rf[2].extended && rf[3].extended && rf[4].extended){
-
-          var xdistL = lf[0].tipPosition[0] - lf[1].tipPosition[0];
-          var ydistL = lf[0].tipPosition[1] - lf[1].tipPosition[1];
-          var zdistL = lf[0].tipPosition[2] - lf[1].tipPosition[2];
-          var lDist = Math.sqrt( xdistL*xdistL + ydistL*ydistL + zdistL*zdistL );
-
-          var xdistR = rf[0].tipPosition[0] - rf[1].tipPosition[0];
-          var ydistR = rf[0].tipPosition[1] - rf[1].tipPosition[1];
-          var zdistR = rf[0].tipPosition[2] - rf[1].tipPosition[2];
-          var rDist = Math.sqrt( xdistR*xdistR + ydistR*ydistR + zdistR*zdistR );
-
-
-          if ( lDist < 20 && rDist < 20 ){
-            frameString += 'YOU MADE THE LETTER "B" MOUTHFUCKER' + dist + '<br>';
-            text = 'B'
+        else if (rf[1].extended) {
+          var xdist = lf[2].tipPosition[0] - rf[1].tipPosition[0];
+          var ydist = lf[2].tipPosition[1] - rf[1].tipPosition[1];
+          var zdist = lf[2].tipPosition[2] - rf[1].tipPosition[2];
+          var dist = Math.sqrt( xdist*xdist + ydist*ydist + zdist*zdist );
+  console.log('i');
+          if ( dist < 20 ){
+            console.log('i here');
+            frameString += 'YOU MADE THE LETTER "I" MOUTHFUCKER' + dist + '<br>';
+            text = 'I'
             voicePlay(text)
           }
-        } //LETTER B
+        } //LETTER I
 
-        // else if (//LETTER D
-        // lf[1].extended && rf[0].extended && rf[1].extended
-        // ){
-        //   var xdistI = lf[0].tipPosition[0] - rf[1].tipPosition[0];
-        //   var ydistI = lf[0].tipPosition[1] - rf[1].tipPosition[1];
-        //   var zdistI = lf[0].tipPosition[2] - rf[1].tipPosition[2];
-        //   var iDist = Math.sqrt( xdistI*xdistI + ydistI*ydistI + zdistI*zdistI );
-        //
-        //   var xdistT = lf[0].tipPosition[0] - rf[1].tipPosition[0];
-        //   var ydistT = lf[0].tipPosition[1] - rf[1].tipPosition[1];
-        //   var zdistT = lf[0].tipPosition[2] - rf[1].tipPosition[2];
-        //   var tDist = Math.sqrt( xdistT*xdistT + ydistT*ydistT + zdistT*zdistT );
-        //
-        //
-        //   if ( lDist < 20 && rDist < 20 ){
-        //     frameString += 'YOU MADE THE LETTER "D" MOUTHFUCKER' + dist + '<br>';
-        //     text = 'D'
-        //     voicePlay(text)
-        //   }
-        // }
-    } else if ((l && !r) || (!l && r)){
-      var hand = frame.hands[0];
+        else if (rf[1].extended){
 
-        if ( hand.fingers[2].extended && !hand.fingers[0].extended && !hand.fingers[1].extended && !hand.fingers[3].extended && !hand.fingers[4].extended) /*If someone flipped the bird*/{
-            $("#celine").attr("src", $("#celine").attr("src").replace("autoplay=0", "autoplay=1"));
-
-        } else if ( //LETTER C
-        hand.fingers[0].extended && hand.fingers[1].extended && !hand.fingers[2].extended && !hand.fingers[3].extended && !hand.fingers[4].extended
-        ) {
-          var xdist = hand.fingers[0].tipPosition[0] - hand.fingers[1].tipPosition[0];
-          var ydist = hand.fingers[0].tipPosition[1] - hand.fingers[1].tipPosition[1];
-          var zdist = hand.fingers[0].tipPosition[2] - hand.fingers[1].tipPosition[2];
+          var xdist = lf[3].tipPosition[0] - rf[1].tipPosition[0];
+          var ydist = lf[3].tipPosition[1] - rf[1].tipPosition[1];
+          var zdist = lf[3].tipPosition[2] - rf[1].tipPosition[2];
           var dist = Math.sqrt( xdist*xdist + ydist*ydist + zdist*zdist );
+  console.log('o');
+          if ( dist < 20 ){
+            frameString += 'YOU MADE THE LETTER "O" MOUTHFUCKER' + dist + '<br>';
+            text = 'O'
+            voicePlay(text)
+          }
+        } //LETTER O
 
-            if ( dist > 70 && dist < 105 ){
-              frameString += 'YOU MADE THE LETTER "C" MOUTHFUCKER' + dist + '<br>';
-              text = 'C'
-              voicePlay(text)
-            }
-        } else if ( //HELLO
-        hand.fingers[0].extended && hand.fingers[1].extended && hand.fingers[2].extended && hand.fingers[3].extended && hand.fingers[4].extended
-        ) {
-          $("#oh-hi-mark").attr("src", $("#oh-hi-mark").attr("src").replace("autoplay=0", "autoplay=1&start=6"));
-        }
+        else if (rf[1].extended){
+
+          var xdist = lf[4].tipPosition[0] - rf[1].tipPosition[0];
+          var ydist = lf[4].tipPosition[1] - rf[1].tipPosition[1];
+          var zdist = lf[4].tipPosition[2] - rf[1].tipPosition[2];
+          var dist = Math.sqrt( xdist*xdist + ydist*ydist + zdist*zdist );
+  console.log('u');
+          if ( dist < 20 ){
+            frameString += 'YOU MADE THE LETTER "U" MOUTHFUCKER' + dist + '<br>';
+            text = 'U'
+            voicePlay(text)
+          }
+        } //LETTER U
       }
 
+      else if ( lf[2].extended && lf[3].extended && lf[4].extended && rf[2].extended && rf[3].extended && rf[4].extended){
+
+        var xdistL = lf[0].tipPosition[0] - lf[1].tipPosition[0];
+        var ydistL = lf[0].tipPosition[1] - lf[1].tipPosition[1];
+        var zdistL = lf[0].tipPosition[2] - lf[1].tipPosition[2];
+        var lDist = Math.sqrt( xdistL*xdistL + ydistL*ydistL + zdistL*zdistL );
+
+        var xdistR = rf[0].tipPosition[0] - rf[1].tipPosition[0];
+        var ydistR = rf[0].tipPosition[1] - rf[1].tipPosition[1];
+        var zdistR = rf[0].tipPosition[2] - rf[1].tipPosition[2];
+        var rDist = Math.sqrt( xdistR*xdistR + ydistR*ydistR + zdistR*zdistR );
+
+
+        if ( lDist < 20 && rDist < 20 ){
+          frameString += 'YOU MADE THE LETTER "B" MOUTHFUCKER' + dist + '<br>';
+          text = 'B'
+          voicePlay(text)
+        }
+      } //LETTER B
+
+      else if (
+      lf[1].extended && rf[0].extended && rf[1].extended
+      ){
+        var xdistI = lf[1].tipPosition[0] - rf[1].tipPosition[0];
+        var ydistI = lf[1].tipPosition[1] - rf[1].tipPosition[1];
+        var zdistI = lf[1].tipPosition[2] - rf[1].tipPosition[2];
+        var iDist = Math.sqrt( xdistI*xdistI + ydistI*ydistI + zdistI*zdistI );
+
+        var xdistT = lf[1].mcpPosition[0] - rf[0].tipPosition[0];
+        var ydistT = lf[1].mcpPosition[1] - rf[0].tipPosition[1];
+        var zdistT = lf[1].mcpPosition[2] - rf[0].tipPosition[2];
+        var tDist = Math.sqrt( xdistT*xdistT + ydistT*ydistT + zdistT*zdistT );
+
+
+        if ( iDist < 20 && tDist < 50 && tDist > 15 ){
+          frameString += 'YOU MADE THE LETTER "D" MOUTHFUCKER' + dist + '<br>';
+          text = 'D'
+          voicePlay(text)
+        }
+      } //LETTER D
+    }//Close both hands visible if statement
+    else if ((l && !r) || (!l && r)){
+      var hand = frame.hands[0];
+
+      if ( hand.fingers[2].extended && !hand.fingers[0].extended && !hand.fingers[1].extended && !hand.fingers[3].extended && !hand.fingers[4].extended) /*If someone flipped the bird*/{
+          $("#celine").attr("src", $("#celine").attr("src").replace("autoplay=0", "autoplay=1"));
+
+      }
+      else if (hand.fingers[0].extended && hand.fingers[1].extended && !hand.fingers[2].extended && !hand.fingers[3].extended && !hand.fingers[4].extended) {
+
+        var xdist = hand.fingers[0].tipPosition[0] - hand.fingers[1].tipPosition[0];
+        var ydist = hand.fingers[0].tipPosition[1] - hand.fingers[1].tipPosition[1];
+        var zdist = hand.fingers[0].tipPosition[2] - hand.fingers[1].tipPosition[2];
+        var dist = Math.sqrt( xdist*xdist + ydist*ydist + zdist*zdist );
+
+          if ( dist > 70 && dist < 105 ){
+            frameString += 'YOU MADE THE LETTER "C" MOUTHFUCKER' + dist + '<br>';
+            text = 'C'
+            voicePlay(text)
+          }
+      } //LETTER C
+      // else if (hand.fingers[0].extended && hand.fingers[1].extended && hand.fingers[2].extended && hand.fingers[3].extended && hand.fingers[4].extended
+      // ) {
+      //   $("#oh-hi-mark").attr("src", $("#oh-hi-mark").attr("src").replace("autoplay=0", "autoplay=1&start=6"));
+      // } //HELLO
+    } //Close single hand only if statement
 
     for (var i = 0; i < frame.hands.length; i++) {
 
@@ -180,5 +196,176 @@ $( document ).ready(function() {
     } //close frame.hands.length loop
 
     output.innerHTML = frameString;
-  }) //close Leap.Loop
-}) //close document ready
+  };
+
+
+    // http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+  function getParam(name) {
+      name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+          results = regex.exec(location.search);
+      return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  };
+    var controller, cursor, initScene, riggedHand, stats;
+
+    window.scene = null;
+
+    window.renderer = null;
+
+    window.camera = null;
+
+    initScene = function(element) {
+      var axis, pointLight;
+      window.scene = new THREE.Scene();
+      window.renderer = new THREE.WebGLRenderer({
+        alpha: true
+      });
+
+      console.log('renderer', window.renderer);
+
+      renderer.setClearColor(0x2F394D, 1);
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      element.appendChild(renderer.domElement);
+      axis = new THREE.AxisHelper(40);
+      scene.add(axis);
+      scene.add(new THREE.AmbientLight(0x888888));
+      pointLight = new THREE.PointLight(0xFFffff);
+      pointLight.position = new THREE.Vector3(-20, 10, 0);
+      pointLight.lookAt(new THREE.Vector3(0, 0, 0));
+      scene.add(pointLight);
+      window.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+      var x = -30;
+      var y = 15;
+      var z = 20;
+      camera.position.set(x, y, z);
+      camera.lookAt(new THREE.Vector3(0, 0, 0));
+      window.controls = new THREE.TrackballControls(camera);
+      scene.add(camera);
+      window.addEventListener('resize', function() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        controls.handleResize();
+        return renderer.render(scene, camera);
+      }, false);
+      return renderer.render(scene, camera);
+    };
+
+    // via Detector.js:
+  var webglAvailable  = ( function () { try { var canvas = document.createElement( 'canvas' ); return !! window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ); } catch( e ) { return false; } } )();
+
+    if (webglAvailable) {
+      console.log('webgl init');
+      initScene(document.getElementById('hands'));
+    }
+    //
+    // stats = new Stats();
+    //
+    // stats.domElement.style.position = 'absolute';
+    //
+    // stats.domElement.style.left = '0px';
+    //
+    // stats.domElement.style.top = '0px';
+    //
+    // document.body.appendChild(stats.domElement);
+
+    window.controller = controller = new Leap.Controller;
+
+    controller.use('handHold').use('transform', {
+      position: new THREE.Vector3(1, 0, 0)
+    }).use('handEntry').use('screenPosition').use('riggedHand', {
+      parent: scene,
+      renderer: renderer,
+      scale: getParam('scale'),
+      positionScale: getParam('positionScale'),
+      helper: true,
+      offset: new THREE.Vector3(0, 0, 0),
+      renderFn: function() {
+        renderer.render(scene, camera);
+        return controls.update();
+      },
+      materialOptions: {
+        wireframe: getParam('wireframe')
+      },
+      dotsMode: getParam('dots'),
+      // stats: stats,
+      camera: camera,
+      boneLabels: function(boneMesh, leapHand) {
+        if (boneMesh.name.indexOf('Finger_03') === 0) {
+          return leapHand.pinchStrength;
+        }
+      },
+      boneColors: function(boneMesh, leapHand) {
+        if ((boneMesh.name.indexOf('Finger_0') === 0) || (boneMesh.name.indexOf('Finger_1') === 0)) {
+          return {
+            hue: 0.6,
+            saturation: leapHand.pinchStrength
+          };
+        }
+      },
+      checkWebGL: true
+    })
+    .on('frame', processFrame)
+    .connect();
+
+    if (getParam('screenPosition')) {
+      cursor = document.createElement('div');
+      cursor.style.width = '50px';
+      cursor.style.height = '50px';
+      cursor.style.position = 'absolute';
+      cursor.style.zIndex = '10';
+      cursor.style.backgroundColor = 'green';
+      cursor.style.opacity = '0.8';
+      cursor.style.color = 'white';
+      cursor.style.fontFamily = 'curior';
+      cursor.style.textAlign = 'center';
+      cursor.innerHTML = "&lt;div&gt;";
+      document.body.appendChild(cursor);
+      controller.on('frame', function(frame) {
+        var hand, handMesh, screenPosition;
+        if (hand = frame.hands[0]) {
+          handMesh = frame.hands[0].data('riggedHand.mesh');
+          screenPosition = handMesh.screenPosition(hand.fingers[1].tipPosition, camera);
+          cursor.style.left = screenPosition.x;
+          return cursor.style.bottom = screenPosition.y;
+        }
+      });
+    }
+
+    if (getParam('scenePosition')) {
+      window.sphere = new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial(0x0000ff));
+      scene.add(sphere);
+      controller.on('frame', function(frame) {
+        var hand, handMesh;
+        if (hand = frame.hands[0]) {
+          handMesh = frame.hands[0].data('riggedHand.mesh');
+          return handMesh.scenePosition(hand.indexFinger.tipPosition, sphere.position);
+        }
+      });
+    }
+
+    if (getParam('playback')) {
+      controller.use('playback', {
+        recording: 'examples/confidence2-49fps.json.lz',
+        autoPlay: true,
+        pauseOnHand: true
+      });
+    }
+
+    if (getParam('boneHand')) {
+      riggedHand = controller.plugins.riggedHand;
+      controller.use('boneHand', {
+        renderer: riggedHand.renderer,
+        scene: riggedHand.parent,
+        camera: riggedHand.camera,
+        render: function() {}
+      });
+    }
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+   //close Leap.Loop
+}); //close document ready
